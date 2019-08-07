af3425a68009f26e3229778e63971082cb67303f
Ehirim Emeka <bluegrayonics@gmail.com>
2019-08-07 14:35:37 +0100

    lol

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -4,7 +4,14 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Review extends Model
 {
-    //
+	protected $fillable = [
+		'star','customer','review'
+	];
+    public function product()
+    {
+    	return $this->belongsTo(Product::class);
+    }
 }
